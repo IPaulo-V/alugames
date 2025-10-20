1e394e4b64f4b27389a3ed747f6356d1d1234e7b
IPaulo-V <paulo.victor_sv@proton.me>
2025-10-20 18:58:34 -0300

    Status botão 2 e 3

--- js/app.js
+++ js/app.js
@@ -8,6 +8,26 @@ function alterarStatus1() {
         buttonSwap('btn1');
     }
 }
+function alterarStatus2() {
+    const link = document.getElementById('btn2');
+    if(link.textContent == 'Devolver') {
+        link.textContent = 'Alugar';
+        buttonSwap('btn2');
+    } else {
+        link.textContent = 'Devolver';
+        buttonSwap('btn2');
+    }
+}
+function alterarStatus3() {
+    const link = document.getElementById('btn3');
+    if(link.textContent == 'Devolver') {
+        link.textContent = 'Alugar';
+        buttonSwap('btn3');
+    } else {
+        link.textContent = 'Devolver';
+        buttonSwap('btn3');
+    }
+}
 function buttonSwap(btn) {
     let botao = document.getElementById(btn);
     if (botao.classList.contains('dashboard__item__button--return')) {
