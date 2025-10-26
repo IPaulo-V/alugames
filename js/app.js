2e8d804ba2fc878f0f55edae1354e998ee6ce4ae
IPaulo-V <paulo.victor_sv@proton.me>
2025-10-26 10:11:16 -0300

    Alertas

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 function alterarStatus(id) {
-    const gameElement = document.getElementById(`game-${id}`);
-    const imageDiv = gameElement.querySelector('.dashboard__item__img');
-    const button = gameElement.querySelector('.dashboard__item__button');
+    let gameElement = document.getElementById(`game-${id}`);
+    let imageDiv = gameElement.querySelector('.dashboard__item__img');
+    let button = gameElement.querySelector('.dashboard__item__button');
+    let nomeDoJogo = gameElement.querySelector('.dashboard__item__name').textContent;
+
 
     if (button.classList.contains('dashboard__item__button--return')) {
         button.classList.remove('dashboard__item__button--return');
         imageDiv.classList.remove('dashboard__item__img--rented');
         button.textContent = 'Alugar';
+        alert(`O game ${nomeDoJogo} foi devolvido.`);
     } else {
         button.classList.add('dashboard__item__button--return');
         imageDiv.classList.add('dashboard__item__img--rented');
         button.textContent = 'Devolver';
+        alert(`O game ${nomeDoJogo} foi alugado.`);
     }
 }
