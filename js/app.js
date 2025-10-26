096fb2c3248a115dea2712bd965a3c147809dc74
IPaulo-V <paulo.victor_sv@proton.me>
2025-10-26 12:26:37 -0300

    confirmação aluguel e devolução

--- js/app.js
+++ js/app.js
@@ -1,23 +1,30 @@
+let quantidadeAlugados = 0;
+
 function alterarStatus(id) {
     let gameElement = document.getElementById(`game-${id}`);
     let imageDiv = gameElement.querySelector('.dashboard__item__img');
     let button = gameElement.querySelector('.dashboard__item__button');
     let nomeDoJogo = gameElement.querySelector('.dashboard__item__name').textContent;
 
-
     if (button.classList.contains('dashboard__item__button--return')) {
-       let confirmacao = confirm(`Deseja devolver o game ${nomeDoJogo}?`);
-        if (!confirmacao) {
+       let confirmaDevolucao= confirm(`Deseja devolver o game ${nomeDoJogo}?`);
+        if (!confirmaDevolucao) {
             return;
         }
         button.classList.remove('dashboard__item__button--return');
         imageDiv.classList.remove('dashboard__item__img--rented');
         button.textContent = 'Alugar';
+        quantidadeAlugados--;
         alert(`O game ${nomeDoJogo} foi devolvido.`);
     } else {
+        let confirmaAluguel= confirm(`Deseja alugar o game ${nomeDoJogo}?`);
+        if (!confirmaAluguel) {
+            return;
+        }
+        quantidadeAlugados++;
         button.classList.add('dashboard__item__button--return');
         imageDiv.classList.add('dashboard__item__img--rented');
         button.textContent = 'Devolver';
-        alert(`O game ${nomeDoJogo} foi alugado.`);
     }
+    alert(`Total de games alugados: ${quantidadeAlugados}`);
 }
